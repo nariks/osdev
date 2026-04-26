66ffe3bf333b564f6fe109bc5b63da7bb33d0b25
Narayan D <happycow@gmail.com>
2026-04-27 09:16:24 +1000

    fixing code review changes in fdt.c

--- kernel/fdt.c
+++ kernel/fdt.c
@@ -11,33 +11,34 @@
 
 uint32_t fdt32_to_cpu(uint32_t value) {
     uint32_t nibbles[BYTES_PER_U32];
-    uint32_t swapped_addr = 0;
+    uint32_t result = 0;
    
     for (int i = 0; i < BYTES_PER_U32; i++) {
         int right_shift = CHAR_BIT * i;
         int left_shift = CHAR_BIT * (BYTES_PER_U32 - (i+1));
         nibbles[i] = (value >> right_shift) & LOBYTE_MASK;
-        swapped_addr |= nibbles[i] << left_shift;
+        result |= nibbles[i] << left_shift;
     }
 
-    return swapped_addr; }
+    return result; 
+}
 
 uint64_t fdt64_to_cpu(uint64_t value) {
     uint64_t octets[BYTES_PER_U64];
-    uint64_t swapped_addr = 0;
+    uint64_t result = 0;
     
     for (int i = 0; i < BYTES_PER_U64; i++) {
         int right_shift = CHAR_BIT * i;
-        int left_shift = CHAR_BIT * (BYTES_PER_U32 - (i+1));
+        int left_shift = CHAR_BIT * (BYTES_PER_U64 - (i+1));
         octets[i] = (value >> right_shift) & LOBYTE_MASK;
-        swapped_addr |= octets[i] << left_shift;
+        result |= octets[i] << left_shift;
     }
 
-    return swapped_addr;
+    return result;
 }
 
 int check_fdt_magic(uintptr_t fdt_address) {
-    const fdt_header *fdt_hdr = (fdt_header *)fdt_address;
+    const fdt_header *fdt_hdr = (const fdt_header *)fdt_address;
     return (fdt32_to_cpu(fdt_hdr->magic) == 0xD00DFEED);
 }
 
